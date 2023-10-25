aa4fb8dd1b9d058064b611418392dd183806d3d6
Chuks Festus <cutogu@gmail.com>
2023-10-25 13:56:53 +0100

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+// import styles from "./page.module.css";
+
+export default function Home() {
+	return <Image src="/logo.svg" alt="Chuks Festus Logo" width={175.15} height={144.59} priority />;
+}
